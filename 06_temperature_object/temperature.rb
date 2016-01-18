c0d58bc53ea0dff7dde30e82669dced3d585cf5f
karenjho <hokarenj@gmail.com>
2016-01-18 18:26:41 -0500

    Create Celsius and Fahrenheit as subclasses of Temperature

--- 06_temperature_object/temperature.rb
+++ 06_temperature_object/temperature.rb
@@ -7,18 +7,30 @@ attr_accessor :f, :c
   end
 
   def to_fahrenheit
-    @c * 9 / 5 + 32
+    @c.to_i * 9 / 5 + 32
   end
 
   def to_celsius
-    ( @f - 32 ) * 5 / 9
+    ( @f.to_i - 32 ) * 5 / 9
   end
 
   def self.in_celsius(temp)
-    @c = temp
+    Temperature.new(:c => temp)
   end
 
   def self.in_fahrenheit(temp)
+    Temperature.new(:f => temp)
+  end
+end
+
+class Celsius < Temperature
+  def initialize(temp)
+    @c = temp
+  end
+end
+
+class Fahrenheit < Temperature
+  def initialize(temp)
     @f = temp
   end
 end
