f20c8060199010c7a8ba29c8bcbc28ddddd934ee
karenjho <hokarenj@gmail.com>
2016-01-18 16:51:11 -0500

    Add 'ay' to teh end of every word in 'phrase'

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,3 +1,11 @@
-def translate(word)
+def translate(phrase)
+  alphabet = ("a".."z").to_a
+  vowels = [ "a","e","i","o","u" ]
+  consonants = alphabet - vowels
+  qu = [ "qu" ]
 
+  result = phrase.split(" ").map do |word|
+    word + "ay"
+  end
+  result.join(" ")
 end
